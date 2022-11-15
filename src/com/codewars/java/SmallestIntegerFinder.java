d4159488fdf35e85a729be031ed6eb3651b13099
hossam <hossamabdelfattah15@gmail.com>
2022-11-15 06:08:49 +0200

    find the smallest integar using java

---
+++ src/com/codewars/java/SmallestIntegerFinder.java
@@ -0,0 +1,22 @@
+package com.codewars.java;
+
+import java.util.Arrays;
+
+public class SmallestIntegerFinder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x[]= {34, 15, 88, 2};
+		
+		
+
+	}
+	
+	public static int findSmallestInt(int[] args) {
+		Arrays.sort(args);
+		return args[0];
+
+    } 
+
+}
